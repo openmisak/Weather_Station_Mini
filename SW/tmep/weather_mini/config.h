1a45c3df9f8084d345c8e1f368386c551ff094c0
Michal Teissing <michal.teissing@gmail.com>
2024-11-04 22:39:20 +0100

    Překopání projektu

--- SW/tmep/weather_mini/config.h
+++ SW/tmep/weather_mini/config.h
@@ -1,10 +1,44 @@
-// vypln tvou domenu cidla, kterou sis zaregistroval na tmep.cz
-// deska 3.2
-//String serverName = "http://4t6jv7-ne224p.tmep.cz/index.php?";
+#ifndef CONFIG_H
+#define CONFIG_H
 
-// vypln tvou domenu cidla, kterou sis zaregistroval na tmep.cz
-//deska v3.5 s čidlem osvitu
-String serverName = "http://apnfk5-v3yn9r.tmep.cz/index.php?";
+/* Definujte typ desky
+3.2 stará deska
+3.5 novější deska
+podle nastavení desky se chová zbytek kódu, posílá se to na správná čidla atd.
+*/
+
+#define ESP32_DEV_v32 1
+#define ESP32_DEV_v35 2
+
+//#define BOARD_TYPE ESP32_DEV_v32
+#define BOARD_TYPE ESP32_DEV_v35
 
 // vypln tve GUID cidla
 String GUID = "tempV";
+String luxGUID = "lux";
+
+//konfigurace ESP32 desky
+#define SLEEP_SEC 15 * 60          // Measurement interval 15 minutes (X * 60 in seconds) 
+#define ADC_PIN 0                  // ADC pin on LaskaKit Meteo mini
+#define deviderRatio 1.7693877551  // Voltage devider ratio on ADC pin 1M + 1.3MOhm (odpovídá schémetu desky)
+#define SDA 19
+#define SCL 18
+#define PIN_ON 3
+
+#if defined(BOARD_TYPE) && BOARD_TYPE == ESP32_DEV_v32
+  #define BME280_ADDRESS (0x76) // adresa čidla deska v3.2 0x76
+  #define TEMP_SENSOR_NAME "http://4t6jv7-ne224p.tmep.cz/index.php?"
+  #define LUX_SENSOR_NAME "http://my9dhv-56c3u9.tmep.cz/index.php?"
+  #define REF_VOLTAGE 3.3 // Float for Reference Voltage, old board 3.3, new version 1
+
+#elif defined(BOARD_TYPE) && BOARD_TYPE == ESP32_DEV_v35
+  #define BME280_ADDRESS (0x77) // adresa čidla deska v3.5 0x77
+  #define  TEMP_SENSOR_NAME "http://apnfk5-v3yn9r.tmep.cz/index.php?"
+  #define LUX_SENSOR_NAME "http://my9dhv-56c3u9.tmep.cz/index.php?"
+  #define REF_VOLTAGE 1 // Float for Reference Voltage, old board 3.3, new version 1
+
+#else
+    #error "Unknown BOARD_TYPE selected"
+#endif
+
+#endif // CONFIG_H
--- SW/tmep/weather_mini/weather_mini.ino
+++ SW/tmep/weather_mini/weather_mini.ino
@@ -1,80 +1,85 @@
-/* LaskaKit DIY Mini Weather Station.  https://www.laskakit.cz/laskakit-meteo-mini-meteostanice/#productDiscussion
- * TMEP edition
- * Read Temperature, Humidity and pressure and send to TMEP.cz
- * 
- * For settings see config.h
- * 
- * Email:podpora@laskakit.cz
- * Web:laskakit.cz
- * Board: ESP32-C3 Dev Module
- * 
- *
- * TEIM: Změna konfigurace, funguje s tmep.cz, bylo potřeba správně nastavit parametry modulu pro přenos 
- *
+/* 
  */
 
 // připojení knihoven
-#include "config.h"  // change to config.h and fill the file.
+#include "config.h"  // základní konfigurace čidla
 
 #include <Wire.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>  // BME280 by Adafruit
-#include <Adafruit_TSL2561_U.h>
-#include <WiFiManager.h>  // WiFi manager by tzapu https://github.com/tzapu/WiFiManager
+#include <Adafruit_BME280.h>     // BME280 by Adafruit - senzor teploty
+#include <Adafruit_TSL2561_U.h>  // TSL2561 by Adafruit - senzor osvitu
+#include <WiFiManager.h>         // WiFi manager by tzapu https://github.com/tzapu/WiFiManager
 
-#define SLEEP_SEC 15 * 60          // Measurement interval 15 minutes (in seconds) 
-#define ADC_PIN 0                  // ADC pin on LaskaKit Meteo mini
-#define deviderRatio 1.7693877551  // Voltage devider ratio on ADC pin 1M + 1.3MOhm
-#define SDA 19
-#define SCL 18
-#define PIN_ON 3
-
-// Vytvoření instance pro teplotní čidlo | Instance creation
-//#define BME280_ADDRESS (0x76)   // STARA DESKA v3.2 0x76
-#define BME280_ADDRESS (0x77)     // NOVA DESKA v3.5 0x77
+//instance pro teplotní čidlo
 Adafruit_BME280 bme;
 
-// Vytvoření instance pro čidlo osvitu
-Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+// Vytvoření instance pro čidlo osvitu, 24351 je vymyšlené unikátní ID
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 24351);
 
 float temperature;
 float pressure;
 float humidity;
 float bat_voltage;
 int32_t rssi;
-
-// Float for Reference Voltage
-float refVoltage = 3.3;
+float lux;
+bool tempSensor;
+bool luxSensor;
 
 void postData() {
-
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
 
+    //načtení síly signálu
     rssi = WiFi.RSSI();
 
-    //GUID, nasleduje hodnota teploty, pro vlhkost "humV", pro CO2 "CO2", pro napeti baterie "v"
-    String serverPath = serverName + "" + GUID + "=" + temperature + "&humV=" + humidity + "&pressV=" + pressure + "&v=" + bat_voltage + "&rssi=" + rssi;
+    if (tempSensor) {
+      //GUID, nasleduje hodnota teploty, pro vlhkost "humV", pro CO2 "CO2", pro napeti baterie "v"
+      String tempServerPath = String(TEMP_SENSOR_NAME) + GUID + "=" + temperature + "&humV=" + humidity + "&pressV=" + pressure + "&v=" + bat_voltage + "&rssi=" + rssi;
+      Serial.println(tempServerPath);
+      // zacatek http spojeni
+      http.begin(tempServerPath.c_str());
 
-    // zacatek http spojeni
-    http.begin(serverPath.c_str());
+      // http get request
+      int tempHttpResponseCode = http.GET();
 
-    // http get request
-    int httpResponseCode = http.GET();
-
-    if (httpResponseCode > 0) {
-      Serial.print("HTTP odpoved: ");
-      Serial.println(httpResponseCode);
-      String payload = http.getString();
-      Serial.println(payload);
-    } else {
-      Serial.print("Error kod: ");
-      Serial.println(httpResponseCode);
+      if (tempHttpResponseCode > 0) {
+        Serial.print("HTTP odpoved: ");
+        Serial.println(tempHttpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      } else {
+        Serial.print("Error kod: ");
+        Serial.println(tempHttpResponseCode);
+      }
+      // Free resources
+      http.end();
     }
-    // Free resources
-    http.end();
+
+    if (luxSensor) {
+      //GUID je lux
+      String luxServerPath = String(LUX_SENSOR_NAME) + luxGUID + "=" + lux;
+
+      // zacatek http spojeni
+      http.begin(luxServerPath.c_str());
+
+      // http get request
+      int luxHttpResponseCode = http.GET();
+
+      if (luxHttpResponseCode > 0) {
+        Serial.print("HTTP odpoved: ");
+        Serial.println(luxHttpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      } else {
+        Serial.print("Error kod: ");
+        Serial.println(luxHttpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+
   } else
     Serial.println("Wi-Fi odpojeno");
 }
@@ -135,9 +140,28 @@ void readBME() {
   Serial.println("hPa");
 }
 
+// Přečíst data z čidla osvitu TSL2561
+void readTSL() {
+  sensors_event_t event;
+  tsl.getEvent(&event);
+
+  /* Display the results (light is measured in lux) */
+  if (event.light) {
+    lux = event.light;
+    Serial.print(String(lux));
+    Serial.println(" lux");
+  } else {
+    /* If event.light = 0 lux the sensor is probably saturated
+       and no reliable data could be generated! */
+    Serial.println("Sensor overload");
+  }
+  delay(250);
+}
+
 void displaySensorDetails(void) {
   sensor_t sensor;
   tsl.getSensor(&sensor);
+  Serial.println("-- LUX Sensor setup --");
   Serial.println("------------------------------------");
   Serial.print("Sensor:       ");
   Serial.println(sensor.name);
@@ -166,7 +190,7 @@ void configureSensor(void) {
   tsl.enableAutoRange(true); /* Auto-gain ... switches automatically between 1x and 16x */
 
   /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
-  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
+  //tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
   tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS); /* medium resolution and speed   */
   // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
 
@@ -175,22 +199,25 @@ void configureSensor(void) {
   Serial.print("Gain:         ");
   Serial.println("Auto");
   Serial.print("Timing:       ");
-  Serial.println("101 ms");
+  Serial.println("13 ms");
   Serial.println("------------------------------------");
 }
 
 // Měření napětí baterie | Battery voltage measurement
 void readBat() {
-  bat_voltage = ((analogReadMilliVolts(ADC_PIN) * deviderRatio) / 1000) * refVoltage;
+  bat_voltage = ((analogReadMilliVolts(ADC_PIN) * deviderRatio) / 1000) * REF_VOLTAGE;
   Serial.println("Battery voltage " + String(bat_voltage) + "V");
 }
 
 void setup() {
+  Serial.println(BOARD_TYPE);
   // Hned vypneme WiFi | disable WiFi, coming from DeepSleep, as we do not need it right away
   WiFi.mode(WIFI_OFF);
   delay(1);
 
-  Serial.begin(115200);
+  //Serial.begin(115200);
+  Serial.begin(9600);
+  delay(100);
   while (!Serial) {}  // Wait
 
   // for board version over 3.5 need to turn uSUP ON
@@ -199,65 +226,63 @@ void setup() {
 
   // initilizace BME280 | BME280 Initialization
   Wire.begin(SDA, SCL);
+
+  //check BME connection
   if (!bme.begin(BME280_ADDRESS)) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1)
-      ;
+    tempSensor = false;
+    //while (1)
+    //  ;
+  } else {
+    tempSensor = true;
+    Serial.println("-- Weather Station setup --");
+    Serial.println("forced mode, 1x temperature / 1x humidity / 1x pressure oversampling,");
+    Serial.println("filter off");
+    bme.setSampling(Adafruit_BME280::MODE_FORCED,
+                    Adafruit_BME280::SAMPLING_X1,  // temperature
+                    Adafruit_BME280::SAMPLING_X1,  // pressure
+                    Adafruit_BME280::SAMPLING_X1,  // humidity
+                    Adafruit_BME280::FILTER_OFF);
+    delay(10);
   }
 
+  //check TSL connection
   if (!tsl.begin()) {
     /* There was a problem detecting the TSL2561 ... check your connections */
     Serial.println("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
-    while (1)
-      ;
-  }
-
-  
-
-  Serial.println("-- Weather Station Scenario --");
-  Serial.println("forced mode, 1x temperature / 1x humidity / 1x pressure oversampling,");
-  Serial.println("filter off");
-  bme.setSampling(Adafruit_BME280::MODE_FORCED,
-                  Adafruit_BME280::SAMPLING_X1,  // temperature
-                  Adafruit_BME280::SAMPLING_X1,  // pressure
-                  Adafruit_BME280::SAMPLING_X1,  // humidity
-                  Adafruit_BME280::FILTER_OFF);
-  delay(10);
-
-  readBME();
-
-  delay(250);
-
-  Serial.println("-- LUX Sensor Scenario --");
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
-  /* Setup the sensor gain and integration time */
-  configureSensor();
-
-  sensors_event_t event;
-  tsl.getEvent(&event);
-
-  /* Display the results (light is measured in lux) */
-  if (event.light) {
-    Serial.print(event.light);
-    Serial.println(" lux");
+    luxSensor = false;
+    //while (1)
+    //  ;
   } else {
-    /* If event.light = 0 lux the sensor is probably saturated
-       and no reliable data could be generated! */
-    Serial.println("Sensor overload");
+    luxSensor = true;
+    /* Display some basic information on this sensor */
+    //displaySensorDetails();
+    /* Setup the sensor gain and integration time */
+    configureSensor();
   }
-  delay(250);
+}
 
+void loop() {
+  //scénář pro senzor osvitu
+  if (luxSensor) {
+    readTSL();
+  }
+
+  //BME read
+  if (tempSensor) {
+    readBME();
+  }
+
+  //BATTERY read
   readBat();
 
   // Pripojeni k WiFi | Connect to WiFi
   WiFiConnection();
+
   postData();
 
   WiFi.disconnect(true);
-  GoToSleep();
-}
 
-void loop() {
-  // Nepotřebujeme loop | We dont use the loop
+  //sleep desky
+  GoToSleep();
 }
